5bc60ad2ff12edd998476b388cce20033880e9a9
SeefatHimel <seefathimel1@gmail.com>
2023-01-27 15:24:34 +0600

    init with add member

---
+++ src/server.js
@@ -0,0 +1,126 @@
+const mongoose = require("mongoose");
+const { signIn } = require("./severModules/login");
+const {
+  GetJwtAccessToken,
+  AuthenticateJwtAccessToken,
+} = require("./services/tokenService");
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const UserTokens = require("./models/userTokens");
+const { GetUserInfo } = require("./services/mongoDBService");
+
+const {
+  GoogleLogin,
+  GetGoogleLoginLink,
+} = require("./severModules/googleSignIn");
+const { CheckEmailValidity, RegisterUser } = require("./severModules/signUp");
+const { AddMember } = require("./severModules/family/addMember");
+const { CreateFamily } = require("./severModules/family");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://himel:himel@cluster0.6uvuj.mongodb.net/familyTree",
+  (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("connected");
+    }
+  }
+);
+
+const corsOptions = {
+  origin: true, //included origin as true
+
+  credentials: true, //included credentials as true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ hello: "hello" });
+});
+
+app.post("/token", async (req, res) => {
+  const refreshToken = req.body.token;
+  await GetJwtAccessToken(refreshToken, res);
+});
+
+app.get("/getLink", (req, res) => {
+  GetGoogleLoginLink(req, res);
+});
+
+app.get("/login", async (req, res) => {
+  const code = req.query.code ? req.query.code : req.body.code;
+  console.log("Code ", code);
+  await GoogleLogin(code, res);
+});
+
+app.get("/getData", AuthenticateJwtAccessToken, async (req, res) => {
+  // console.log(req);
+  await GetUserInfo(req, res);
+});
+
+app.post("/signUp", async (req, res) => {
+  console.log("/signUp : ", req.body);
+  try {
+    const userRegistered = await RegisterUser(req.body.data, res);
+    if (userRegistered) {
+      console.log("/signUp", "Successful !");
+    }
+  } catch (error) {
+    console.log("signUp catch error > ", error);
+    console.log("Failed to signUp.");
+    res.status(400).send({
+      message: "Failed to signUp.",
+    });
+  }
+});
+
+app.post("/signIn", async (req, res) => {
+  await signIn(req, res);
+});
+
+app.post("/register_email", async (req, res) => {
+  CheckEmailValidity(req, res);
+});
+
+app.post("/logout", async (req, res) => {
+  const tokens = await UserTokens.find().clone();
+  if (tokens[0]) {
+    try {
+      await UserTokens.deleteMany({});
+      console.log("Refresh Tokens Deleted");
+      res.status(200).send({ message: "Logged out!!" });
+    } catch (e) {
+      console.log(e.message);
+      res.status(400).send({ message: "Error!!" });
+    }
+  } else res.status(200).send({ message: "Logged out!!" });
+});
+
+app.post("/familyTree/enter", async (req, res) => {
+  CreateFamily(req, res);
+  console.log("🚀 ~ file: server.js:111 ~ app.post ~ req.body", req.body);
+  // res.status(200).send({ message: "Enter!!" });
+});
+
+app.post("/familyTree/add", async (req, res) => {
+  console.log(req.body);
+  try {
+    await AddMember(req, res);
+  } catch (error) {
+    console.log("🚀 ~ file: server.js:111 ~ app.post ~ error", error);
+    res.status(400).send({ message: "Error!!" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server running");
+});
---
+++ src/severModules/family/index.js
@@ -0,0 +1,103 @@
+const FamilyList = require("../../models/familyList");
+const { v4: uuidv4 } = require("uuid");
+
+async function CreateFamily(req, res) {
+  const data = req.body;
+  console.log("🚀 ~ file: index.js:6 ~ CreateFamily ~ data", data);
+  const oldFamily = await FamilyList.where("name").equals(data.name);
+  if (oldFamily && oldFamily[0]) {
+    console.log("Name already Exists");
+    await GetFamily(req, res);
+  } else {
+    const newFamily = new FamilyList();
+    newFamily.name = data.name;
+    newFamily.id = uuidv4();
+    newFamily.password = data.password;
+    newFamily.setPassword(data.password);
+
+    // Save newFamily object to database
+    try {
+      newFamily.save((err, FamilyList) => {
+        if (err) {
+          console.log(err);
+          console.log("Failed to add Family.");
+          return res.status(400).send({
+            message: "Failed to add Family.",
+          });
+        } else {
+          console.log("Family added successfully.");
+          return res.status(201).send({
+            message: "Family added successfully.",
+            id: newFamily.id,
+            details: {
+              members: [],
+              name: newFamily.name,
+            },
+          });
+        }
+      });
+    } catch (error) {
+      console.log("error > ", error);
+      console.log("Failed to add Family.");
+      return res.status(400).send({
+        message: "Failed to add Family.",
+      });
+    }
+  }
+}
+
+async function GetFamily(req, res) {
+  console.log("Name : ", req.body.name, " Pass : ", req.body.password);
+  if (!req.body.name) {
+    console.log("Request name was empty.");
+    return res.status(400).send({
+      message: "Request name was empty.",
+    });
+  }
+  try {
+    await FamilyList.findOne(
+      { name: req.body.name },
+      async function (err, family) {
+        if (err) console.error(err);
+        if (!family) {
+          console.log("FamilyList not found.");
+          return res.status(400).send({
+            message: "FamilyList not found.",
+          });
+        } else {
+          console.log("family > ", family);
+          if (family.validPassword(req.body.password)) {
+            const Members = require("../../models/familyMember")(family.id);
+            const members = await Members.find();
+
+            res.cookie("activeFamilyID", family?.id, {
+              secure: true,
+              sameSite: "strict",
+            });
+            console.log("...............................");
+            res.send({
+              id: family?.id,
+              details: {
+                name: family?.name,
+                members: members,
+              },
+              message: "Logged in successfully",
+            });
+            console.log("...............................");
+          } else {
+            return res.status(400).send({
+              message: "Wrong Password",
+            });
+          }
+        }
+      }
+    ).clone();
+  } catch (error) {
+    console.log("Sign in Error : ", error);
+    return res.status(400).send({
+      message: "FamilyList not found.",
+    });
+  }
+}
+
+module.exports = { CreateFamily };
