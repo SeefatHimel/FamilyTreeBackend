76204dfdee63cdc04a8b7dc89f0b25e91b592080
SeefatHimel <seefathimel1@gmail.com>
2023-05-03 12:29:44 +0600

    remove : fileUpload , add : authGuard

--- src/server.js
+++ src/server.js
@@ -24,21 +24,7 @@ const {
   UpdateMember,
   AddOriginMember,
 } = require("./severModules/family/memberActions");
-const {
-  CreateFamily,
-  GetFamilyMembers,
-  MakeImgLinkImage,
-} = require("./severModules/family");
-const {
-  UploadImage,
-  upload,
-  GetImage,
-} = require("./severModules/imageUpload/imageUpload.action");
-const {
-  UploadToGDrive,
-  GetGDrivePictures,
-  CheckGDrivePictures,
-} = require("./severModules/gdrive");
+const { CreateFamily, GetFamilyMembers } = require("./severModules/family");
 
 const app = express();
 
@@ -127,18 +113,21 @@ app.post("/logout", async (req, res) => {
   } else res.status(200).send({ message: "Logged out!!" });
 });
 
-app.post("/familyTree/enter", async (req, res) => {
+app.post("/familyTree/enter", AuthenticateJwtAccessToken, async (req, res) => {
   CreateFamily(req, res);
   console.log("🚀 ~ file: server.js:121 ~ app.post ~ req.body", req.body);
   // res.status(200).send({ message: "Enter!!" });
 });
 
-app.get("/familyTree/getDetails", async (req, res) => {
-  GetFamilyMembers(req, res);
-  // MakeImgLinkImage(req, res);
-});
+app.get(
+  "/familyTree/getDetails",
+  AuthenticateJwtAccessToken,
+  async (req, res) => {
+    GetFamilyMembers(req, res);
+  }
+);
 
-app.post("/familyTree/add",  async (req, res) => {
+app.post("/familyTree/add", AuthenticateJwtAccessToken, async (req, res) => {
   console.log("/familyTree/add", req.body, typeof req.body.data);
 
   try {
@@ -149,17 +138,21 @@ app.post("/familyTree/add",  async (req, res) => {
     res.status(400).send({ message: "Error!!" });
   }
 });
-app.post("/familyTree/add/origin", async (req, res) => {
-  console.log(req.body);
-  try {
-    await AddOriginMember(req, res);
-  } catch (error) {
-    console.log("🚀 ~ file: server.js:143 ~ app.post ~ error", error);
-    res.status(400).send({ message: "Error!!" });
+app.post(
+  "/familyTree/add/origin",
+  AuthenticateJwtAccessToken,
+  async (req, res) => {
+    console.log(req.body);
+    try {
+      await AddOriginMember(req, res);
+    } catch (error) {
+      console.log("🚀 ~ file: server.js:143 ~ app.post ~ error", error);
+      res.status(400).send({ message: "Error!!" });
+    }
   }
-});
+);
 
-app.put("/familyTree/update", async (req, res) => {
+app.put("/familyTree/update", AuthenticateJwtAccessToken, async (req, res) => {
   console.log(req.body);
   try {
     await UpdateMember(req, res);
@@ -169,7 +162,7 @@ app.put("/familyTree/update", async (req, res) => {
   }
 });
 
-app.post("/familyTree/delete", async (req, res) => {
+app.post("/familyTree/delete", AuthenticateJwtAccessToken, async (req, res) => {
   console.log(req.body);
   try {
     await DeleteMember(req, res);
@@ -179,18 +172,6 @@ app.post("/familyTree/delete", async (req, res) => {
   }
 });
 
-app.post("/upload", upload.single("image"), async (req, res) => {
-  await UploadImage(req, res);
-});
-app.get("/download", async (req, res) => {
-  await GetImage(req, res);
-});
-app.get("/files", async (req, res) => {
-  await GetGDrivePictures();
-});
-
 app.listen(3000, () => {
-  // UploadToGDrive();
-  // GetGDrivePictures();
   console.log("Server running");
 });
